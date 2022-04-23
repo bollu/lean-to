61d8290502e1e014c3641f229a70c950452167c1
Siddharth Bhat <siddu.druid@gmail.com>
2022-04-23 23:34:50 +0100

    consolidate sockets into array

--- asm-kernel.cpp
+++ asm-kernel.cpp
@@ -20,6 +20,16 @@ std::string uuid4() {
     return "78d25e41-171d-494b-92f2-4fce84fb3524";
 }
 using ll = long long;
+
+enum SocketKind {
+    HEARTBEAT,
+    IOPUB,
+    CONTROL,
+    STDIN,
+    SHELL,
+    NSOCKETKIND
+};
+
 int main(int argc, char **argv) {
     // TODO: need to parse argv for json file.
     assert(argc == 2  && "expected config JSON file path");
@@ -56,22 +66,23 @@ int main(int argc, char **argv) {
     void *ctx = zmq_ctx_new ();
     // zmq::context_t ctx{1};
 
+    void *sockets[NSOCKETKIND];
     // ##########################################
     // # Heartbeat:
-    void *heartbeat_socket = zmq_socket(ctx, ZMQ_REP);
+    sockets[HEARTBEAT] = zmq_socket(ctx, ZMQ_REP);
     {
         const std::string s = connection + ":" + heartbeat_port;
-        rc = zmq_bind(heartbeat_socket, s.c_str());
+        rc = zmq_bind(sockets[HEARTBEAT], s.c_str());
         assert(rc == 0);
     }
 
     // ##########################################
     // # IOPub/Sub
     // # also called SubSocketChannel in IPython sources
-    void *iopub_socket = zmq_socket(ctx, ZMQ_PUB);
+    sockets[IOPUB] = zmq_socket(ctx, ZMQ_PUB);
     {
         const std::string s = connection + ":" + iopub_port;
-        rc = zmq_bind(iopub_socket, s.c_str());
+        rc = zmq_bind(sockets[IOPUB], s.c_str());
         assert(rc == 0);
     }
     // iopub_stream = zmqstream.ZMQStream(iopub_socket)
@@ -80,29 +91,29 @@ int main(int argc, char **argv) {
 
     // ##########################################
     // # Control:
-    void *control_socket = zmq_socket(ctx, ZMQ_ROUTER);
+    sockets[CONTROL] = zmq_socket(ctx, ZMQ_ROUTER);
     {
         const std::string s = connection + ":" + control_port;
-        rc = zmq_bind(control_socket, s.c_str());
+        rc = zmq_bind(sockets[CONTROL], s.c_str());
         assert(rc == 0);
     }
  
 
     // ##########################################
     // # Stdin
-    void *stdin_socket = zmq_socket(ctx, ZMQ_ROUTER);
+    sockets[STDIN] = zmq_socket(ctx, ZMQ_ROUTER);
     {
         const std::string s = connection + ":" + stdin_port;
-        rc = zmq_bind(stdin_socket, s.c_str());
+        rc = zmq_bind(sockets[STDIN], s.c_str());
         assert(rc == 0);
     }
 
     // ##########################################
     // # shell
-    void *shell_socket = zmq_socket(ctx, ZMQ_ROUTER);
+    sockets[SHELL] = zmq_socket(ctx, ZMQ_ROUTER);
     {
         const std::string s = connection + ":" + shell_port;
-        rc = zmq_bind(shell_socket, s.c_str());
+        rc = zmq_bind(sockets[SHELL], s.c_str());
         assert(rc == 0);
     }
 
@@ -110,7 +121,7 @@ int main(int argc, char **argv) {
     std::cout << "[KERNEL] starting polling loop\n";
     // http://api.zeromq.org/master:zmq-poll
     zmq_pollitem_t items [5];
-    items[0].socket = heartbeat_socket;
+    items[0].socket = sockets[HEARTBEAT];
     items[0].events = ZMQ_POLLIN;
     for (;;) {
         // zmq::message_t request;
@@ -123,10 +134,10 @@ int main(int argc, char **argv) {
             zmq_msg_t msg;
             rc = zmq_msg_init (&msg);
             assert (rc == 0);
-            rc = zmq_recvmsg (heartbeat_socket, &msg, 0);
+            rc = zmq_recvmsg (sockets[HEARTBEAT], &msg, 0);
             assert (rc != -1);
             // Release message
-            rc = zmq_msg_send(&msg, heartbeat_socket, 0);
+            rc = zmq_msg_send(&msg, sockets[HEARTBEAT], 0);
             assert(rc != -1);
             zmq_msg_close (&msg);
             
@@ -138,7 +149,7 @@ int main(int argc, char **argv) {
         // std::cout << "Received " << request.to_string() << "\n";
 
         // heartbeat
-        // zmq_device(ZMQ_FORWARDER, heartbeat_socket.handle(), heartbeat_socket.handle());
+        // zmq_device(ZMQ_FORWARDER, sockets[HEARTBEAT].handle(), sockets[HEARTBEAT].handle());
         // std::cout << "Sent heartbeat.\n";
 
         // simulate work
